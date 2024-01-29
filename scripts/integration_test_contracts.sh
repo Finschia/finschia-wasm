9b368d6b9737f47e3a9f47ab419473f63739cfcf
kokeshiM0chi <frogandfrog009@gmail.com>
2024-01-29 15:36:24 +0900

    fix: Get code_id instead of fixed value

--- scripts/integration_test_contracts.sh
+++ scripts/integration_test_contracts.sh
@@ -37,7 +37,8 @@ raw_log=$(echo ${result} | jq .raw_log)
 check_run_info "${raw_log}" "store: "
 
 #*** instantiate collection contract ***
-result=$(fnsad tx wasm instantiate 1 '{"name":"collection_name","uri":"collection_uri","meta":"collection_meta", "owner":"'${CONTRACT_ADDRESS}'"}' --label collection1 --admin ${FROM_ACCOUNT}  --from ${FROM_ACCOUNT} --node ${URL} ${CHAIN_OPTION})
+CODE_ID=$(echo "${result}" | jq '.logs[] | select(.msg_index == 0) | .events[] | select(.type == "store_code") | .attributes[] | select(.key == "code_id") | .value | tonumber')
+result=$(fnsad tx wasm instantiate ${CODE_ID} '{"name":"collection_name","uri":"collection_uri","meta":"collection_meta", "owner":"'${CONTRACT_ADDRESS}'"}' --label collection1 --admin ${FROM_ACCOUNT}  --from ${FROM_ACCOUNT} --node ${URL} ${CHAIN_OPTION})
 
 ## confirm a result of `instantiate`
 raw_log=$(echo ${result} | jq .raw_log)
