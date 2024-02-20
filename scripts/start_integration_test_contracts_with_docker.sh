379eec4a4164a7f3b5d908d53009dda622ad6145
kokeshiM0chi <frogandfrog009@gmail.com>
2024-02-20 11:40:02 +0900

    Revert "fix: download "init_single.sh" before `docker pull` and add some comments"
    This reverts commit 4099ae4e6808ce11b660552437d25e9553bb4eb8.

--- scripts/start_integration_test_contracts_with_docker.sh
+++ scripts/start_integration_test_contracts_with_docker.sh
@@ -2,19 +2,18 @@
 
 # Get the latest Finschia tag
 TAG=$(curl "https://api.github.com/repos/Finschia/finschia/tags" | jq -r '.[0].name')
+TAG=$(echo "$TAG" | cut -c 2-)
+
+TEST_DOCKER_IMAGE=finschia/finschianode:${TAG}
+docker pull finschia/finschianode:${TAG}
+
+export FNSAD="docker run -i --rm -p 26656:26656 -p 26657:26657 -v ${HOME}/.finschia:/root/.finschia ${TEST_DOCKER_IMAGE} fnsad"
 
 # Copy init_single.sh from Finschia/finschia/init_single.sh
 init_single=$(mktemp)
-curl "https://raw.githubusercontent.com/Finschia/finschia/${TAG}/init_single.sh" -o $init_single
-bash -xe $init_single
+curl "https://raw.githubusercontent.com/Finschia/finschia/v${TAG}/init_single.sh" -o $init_single
 
-# Remove v from tag prefix
-# ex. v1.0.0 -> 1.0.0
-TAG=$(echo "$TAG" | cut -c 2-)
-# Download docker image of Finschia/finschia latest tag
-TEST_DOCKER_IMAGE=finschia/finschianode:${TAG}
-docker pull finschia/finschianode:${TAG}
-export FNSAD="docker run -i --rm -p 26656:26656 -p 26657:26657 -v ${HOME}/.finschia:/root/.finschia ${TEST_DOCKER_IMAGE} fnsad"
+bash -xe $init_single
 
 container_id=$(docker run -d -p 26656:26656 -p 26657:26657 -v ${HOME}/.finschia:/root/.finschia ${TEST_DOCKER_IMAGE} fnsad start)
 
