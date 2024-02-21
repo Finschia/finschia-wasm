bb794cf5d6cdfff873d7cbf96d07f27bebaa76ba
loloicci <loloicci@linecorp.com>
2024-02-21 15:47:01 +0900

    fix: use arm64 targeted container in arm64 machine

--- scripts/build_collection.sh
+++ scripts/build_collection.sh
@@ -8,10 +8,16 @@ DOCKER=${DOCKER:-docker}
 # Build collection contract
 WORKSPACE_DIR=$(realpath $(dirname $0)/../)
 
+# if arm64, add it to container name's suffix
+SUFFIX=""
+if [ "$(uname -m)" = "arm64" ]; then
+  SUFFIX="-arm64"
+fi
+
 if [ ! -f "${WORKSPACE_DIR}/artifacts/collection.wasm" ]; then
   echo "##### Build collection.wasm #####" >&2
   ${DOCKER} run --rm -v "${WORKSPACE_DIR}":/code \
     --mount type=volume,source="devcontract_cache_collection",target=/code/examples/contracts/collection/target \
     --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-    cosmwasm/rust-optimizer:0.15.0 ./examples/contracts/collection
+    cosmwasm/rust-optimizer${SUFFIX}:0.15.0 ./examples/contracts/collection
 fi
