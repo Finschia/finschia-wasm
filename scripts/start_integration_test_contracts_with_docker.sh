64e37254a5911a8b1f11a89eab10b3988f6adbea
loloicci <loloicci@linecorp.com>
2024-02-21 13:22:51 +0900

    refactor: enable user can specify docker/jq command

--- scripts/start_integration_test_contracts_with_docker.sh
+++ scripts/start_integration_test_contracts_with_docker.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Commands
+DOCKER=${DOCKER:-docker}
+JQ=${JQ:-jq}
+
 script_dir=$(realpath $(dirname $0))
 
 # Build collection.wasm if not exitst
-. ${script_dir}/build_collection.sh
+echo "##### Build collection.wasm if there is not #####" >&2
+env DOCKER=${DOCKER} ${script_dir}/build_collection.sh
 
 # Get the latest Finschia tag
-TAG=$(curl -s "https://api.github.com/repos/Finschia/finschia/tags" | jq -r '.[0].name')
+TAG=$(curl -s "https://api.github.com/repos/Finschia/finschia/tags" | ${JQ} -r '.[0].name')
 
 # Remove v from tag prefix
 # ex. v1.0.0 -> 1.0.0
@@ -15,7 +20,7 @@ TAG_NUM=$(echo "$TAG" | cut -c 2-)
 echo "##### Pull finschianode docker #####" >&2
 
 TEST_DOCKER_IMAGE=finschia/finschianode:${TAG_NUM}
-docker pull ${TEST_DOCKER_IMAGE}
+${DOCKER} pull ${TEST_DOCKER_IMAGE}
 
 echo "##### Init node for test #####" >&2
 
@@ -28,21 +33,21 @@ env FNSAD="docker run -i --rm -v ${HOME}/.finschia:/root/.finschia ${TEST_DOCKER
 
 echo "##### Start node #####" >&2
 
-container_id=$(docker run -d -v ${HOME}/.finschia:/root/.finschia ${TEST_DOCKER_IMAGE} fnsad start)
+container_id=$(${DOCKER} run -d -v ${HOME}/.finschia:/root/.finschia ${TEST_DOCKER_IMAGE} fnsad start)
 
 echo "##### Install tools to container #####" >&2
 
-docker exec ${container_id} apk add --no-cache jq bash curl &&
+${DOCKER} exec ${container_id} apk add --no-cache jq bash curl &&
 
 echo "##### Install scripts and contracts to container #####" >&2 &&
-docker cp ${script_dir}/integration_test_contracts.sh ${container_id}:/root/integration_test_contracts.sh &&
+${DOCKER} cp ${script_dir}/integration_test_contracts.sh ${container_id}:/root/integration_test_contracts.sh &&
 docker cp ${script_dir}/../artifacts/collection.wasm ${container_id}:/root/ &&
 echo "##### Start tests #####" >&2 &&
-docker exec ${container_id} /root/integration_test_contracts.sh
+${DOCKER} exec ${container_id} /root/integration_test_contracts.sh
 
 echo "##### Tests Succeed #####" >&2
 echo "##### Stop docker #####" >&2
-docker stop ${container_id}
+${DOCKER} stop ${container_id}
 
 # Remove the temporary file `Finschia/init_single.sh`
 rm $init_single
